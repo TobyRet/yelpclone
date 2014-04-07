d830c7966a0e9d4b9d3e2071a53794382f725b24
Toby Retallick <toby.retallick@gmail.com>
2014-04-07 15:50:42 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yelpclone::Application.config.secret_key_base = '5d195e841012704efc57eda0555c53cc551e297b43c3918d6241a1dc321674399970a0a371248009a12453c367eb0d2b31a9263011d48062467ede70e1c29774'
